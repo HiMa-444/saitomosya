cb7ddf22b7ea25cb1ef2f908917458f42da7ba30
EC2 Default User <ec2-user@ip-172-31-20-214.ap-northeast-1.compute.internal>
2023-01-15 15:26:01 +0000

    コードの書き直し

---
+++ サイト模写.js
@@ -0,0 +1,6 @@
+function tabtgl() {
+  let tabtgl = document.getElementById("tab")
+  tabtgl.classList.toggle("cross")
+  let hide = document.getElementById("hide")
+  hide.classList.toggle("view")
+}
